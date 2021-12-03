893521c5177208daddfda9b64a9fe4673dbfbdeb
Raul Martin Catalan <rmartinc96@gmail.com>
2021-12-03 15:09:10 +0100

    First commit

---
+++ app/src/main/java/com/svalero/enjoypadel/MainActivity.java
@@ -0,0 +1,13 @@
+package com.svalero.enjoypadel;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
